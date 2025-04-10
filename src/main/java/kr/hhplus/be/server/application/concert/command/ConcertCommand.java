6b3da0b17dae652c06e0e0302f7bfc559f50d498
Bang <balstn742@gmail.com>
2025-04-11 03:48:13 +0900

    Fix: Concert Domain & Command 변경(class -> record)

--- src/main/java/kr/hhplus/be/server/application/concert/command/ConcertCommand.java
+++ src/main/java/kr/hhplus/be/server/application/concert/command/ConcertCommand.java
@@ -1,42 +1,16 @@
 package kr.hhplus.be.server.application.concert.command;
 
 
-import kr.hhplus.be.server.interfaces.api.common.APIException;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class ConcertCommand {
-    private long concertId;
-    private String name;
-    private LocalDate sDate;
-    private LocalDate eDate;
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
-
-    protected ConcertCommand(
+public record ConcertCommand(
         long concertId,
-        String name,
+        String concertName,
         LocalDate sDate,
         LocalDate eDate,
         LocalDateTime created_at,
         LocalDateTime updated_at
-    ) {}
-
-
-    public static ConcertCommand of(
-            long concertId,
-            String name,
-            LocalDate sDate,
-            LocalDate eDate,
-            LocalDateTime created_at,
-            LocalDateTime updated_at
-    ) {
-        // 시작일 이 종료일 이후일 수 없음
-        if (sDate.isAfter(eDate)) {
-            throw APIException.startDateAfterEndDate();
-        }
-        return new ConcertCommand(concertId, name, sDate, eDate, created_at, updated_at);
-    }
+) {
 }
 
--- src/main/java/kr/hhplus/be/server/domain/concert/domain/Concert.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/domain/Concert.java
@@ -6,37 +6,18 @@ import kr.hhplus.be.server.interfaces.api.common.APIException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Concert {
-    private long concertId;
-    private String name;
-    private LocalDate sDate;
-    private LocalDate eDate;
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
-
-    protected Concert(
+public record Concert(
         long concertId,
-        String name,
+        String concertName,
         LocalDate sDate,
         LocalDate eDate,
         LocalDateTime created_at,
         LocalDateTime updated_at
-    ) {}
-
-
-    public static Concert of(
-            long concertId,
-            String name,
-            LocalDate sDate,
-            LocalDate eDate,
-            LocalDateTime created_at,
-            LocalDateTime updated_at
-    ) {
-        // 시작일 이 종료일 이후일 수 없음
-        if (sDate.isAfter(eDate)) {
+) {
+    public Concert{
+        if (sDate.isAfter(eDate)){
             throw APIException.startDateAfterEndDate();
         }
-        return new Concert(concertId, name, sDate, eDate, created_at, updated_at);
     }
 }
 
