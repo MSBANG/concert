fcc65e877e6b9a1f44cb19e50aed1b3c87f29c4d
Bang <balstn742@gmail.com>
2025-04-11 03:53:42 +0900

    Fix: Controller, DTO 수정(현재 미사용)

--- src/main/java/kr/hhplus/be/server/interfaces/api/concert/dto/ConcertDTO.java
+++ src/main/java/kr/hhplus/be/server/interfaces/api/concert/dto/ConcertDTO.java
@@ -12,19 +12,14 @@ public record ConcertDTO(
         LocalDate sDate,
         LocalDate eDate,
         LocalDateTime created_at,
-        LocalDateTime updated_at,
-        List<ConcertDateDTO> concertDates
+        LocalDateTime updated_at
 ) {
 
     public static ConcertDTO getDefault(){
-        List<ConcertDateDTO> defaultDates = Arrays.asList(
-                ConcertDateDTO.getDefault(),
-                ConcertDateDTO.getDefault()
-        );
         LocalDate sDate = LocalDate.of(2025,4,1);
         LocalDate eDate = LocalDate.of(2025,4,5);
         LocalDateTime created_at = LocalDateTime.of(2025,4,5, 1,1,1);
         LocalDateTime updated_at = LocalDateTime.of(2025,4,5,1,1,1);
-        return new ConcertDTO(1L, "콘서트 이름", sDate, eDate, created_at, updated_at, defaultDates);
+        return new ConcertDTO(1L, "콘서트 이름", sDate, eDate, created_at, updated_at);
     };
 }
