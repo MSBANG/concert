38f0ab11d1aad19308fb1bdfa8153504521410fb
Bang <balstn742@gmail.com>
2025-04-11 03:53:16 +0900

    Remove: 코드 변경으로 인한 기존 class 제거 (ConcertService, ConcertWithDate)

--- src/main/java/kr/hhplus/be/server/application/ConcertService.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.application;
-
-import kr.hhplus.be.server.domain.concert.ConcertRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ConcertService {
-//    ConcertRepository concertRepo =
-}
