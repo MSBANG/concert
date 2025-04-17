e2a88915523fbcc93622637b5585039c7303c06e
Bang <balstn742@gmail.com>
2025-04-17 17:55:25 +0900

    Fix: ReservationRepository 구현체 수정

--- src/main/java/kr/hhplus/be/server/infrastructure/reservation/ReservationJPARepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/reservation/ReservationJPARepositoryImpl.java
@@ -5,12 +5,18 @@ import kr.hhplus.be.server.domain.reservation.ReservationRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ReservationJPARepositoryImpl implements ReservationRepository {
     @Override
     public List<Reservation> getReservationsByUserId(long userId) {
         return List.of();
+    }
+
+    @Override
+    public Optional<Reservation> getReservationByUserIdAndReservationId(long userId, long reservationId) {
+        return Optional.empty();
     }
 
     @Override
