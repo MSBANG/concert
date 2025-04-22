d155883b20289c9ec6386e2c544b0da7d02977bf
Bang <balstn742@gmail.com>
2025-04-22 16:44:39 +0900

    Feat: User 도메인 생성자 추가

--- src/main/java/kr/hhplus/be/server/domain/user/User.java
+++ src/main/java/kr/hhplus/be/server/domain/user/User.java
@@ -5,10 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import kr.hhplus.be.server.domain.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
