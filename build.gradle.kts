e4acda5c6d013a28a816ffc49d3159bcb8bdfbb2
Bang <balstn742@gmail.com>
2025-04-22 14:36:25 +0900

    Feat: JWT 라이브러리 추가, JWT Configuration 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	implementation("org.mapstruct:mapstruct:1.6.3")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	compileOnly("org.projectlombok:lombok:1.18.38")
+
 	annotationProcessor("org.projectlombok:lombok:1.18.38")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
