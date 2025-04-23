4412d4865824b2669b528319e08d49f86fec2306
Bang <balstn742@gmail.com>
2025-04-23 23:44:04 +0900

    Fix: Queue Repository 쿼리 변경

--- src/main/java/kr/hhplus/be/server/infrastructure/queue/QueueJPARepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/queue/QueueJPARepository.java
@@ -2,17 +2,18 @@ package kr.hhplus.be.server.infrastructure.queue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import kr.hhplus.be.server.domain.queue.Queue;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public interface QueueJPARepository extends JpaRepository<Queue, Long> {
     Queue findByUser_userIdAndConcert_concertId(long userId, long concertId);
 
     List<Queue> findAllByConcert_concertId(long concertId);
 
     long findByConcert_concertId(long concertId);
-    long countByConcert_ConcertIdAndInProgressAndQueueIdLessThan(long concertId, boolean isInProgress, long queueId);
-
-    long countByConcert_ConcertIdAndInProgress(long concertId, boolean isInProgress);
+    long countByConcert_ConcertIdAndIsInProgressAndQueueIdLessThan(long concertId, boolean isInProgress, long queueId);
+    long countByConcert_ConcertIdAndIsInProgress(long concertId, boolean isInProgress);
 }
